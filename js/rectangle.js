8f9e452134fc8e27e3aeb83aa70e820ec78105c9
Truman Purnell <truman.purnell@gmail.com>
2018-01-07 17:05:01 -0500

    add nifty value of for easy render

--- js/rectangle.js
+++ js/rectangle.js
@@ -4,13 +4,50 @@ class Rectangle {
         this.B = B
         this.C = C
         this.D = D
+
+        setRetrieval(this.A)
+        setRetrieval(this.B)
+        setRetrieval(this.C)
+        setRetrieval(this.D)
+    }
+
+    setRetrieval(vertex) {
+        vertex.valueOf = function () {
+            return Object.values(vertex)
+        }
     }
 
     extend(point) {
 
     }
 
-    render(context){
-        
+    render(context) {
+        ctx.moveTo(0, 0);
+        ctx.lineTo(200, 100);
+        ctx.stroke();
     }
 }
+
+const rect = new Rectangle(
+    {
+        x: 100,
+        y: 100
+    },
+    {
+        x: 600,
+        y: 100
+    },
+    {
+        x: 100,
+        y: 400
+    },
+    {
+        x: 600,
+        y: 400
+    })
+
+const canvas = document.getElementById('myCanvas')
+const context = canvas.getContext("2d")
+
+rect.render(context)
+
