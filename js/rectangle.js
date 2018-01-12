796f3222b3dd20b669b5872e01125279cf6a55bd
Truman Purnell <truman.purnell@gmail.com>
2018-01-12 11:27:33 -0500

    refactor action code in start.js

--- js/rectangle.js
+++ js/rectangle.js
@@ -1,53 +1,72 @@
+const DIRECTION = {
+    WEST: 0,
+    NORTH: 1,
+    EAST: 2,
+    SOUTH: 3
+}
+
 class Rectangle {
+
     constructor(A, B, C, D) {
         this.A = A
         this.B = B
         this.C = C
         this.D = D
 
-        setRetrieval(this.A)
-        setRetrieval(this.B)
-        setRetrieval(this.C)
-        setRetrieval(this.D)
+        this.context = null
     }
 
-    setRetrieval(vertex) {
-        vertex.valueOf = function () {
-            return Object.values(vertex)
+    setContext(context) {
+        this.context = context
+    }
+
+    locate(vertex) {
+        return Object.values(this[vertex])
+    }
+
+    rotate(degree) { }
+
+    moveVertices(dimension, amount) {
+        Object.keys(this).forEach(function (vertex) {
+            this[vertex][dimension] += amount
+        }, this)
+    }
+
+    shift(dir, amount) {
+        switch (dir) {
+            case DIRECTION.WEST: this.moveVertices('x', -amount)
+                break
+            case DIRECTION.EAST: this.moveVertices('x', amount)
+                break
+            case DIRECTION.NORTH: this.moveVertices('y', -amount)
+                break
+            case DIRECTION.SOUTH: this.moveVertices('y', amount)
+                break
         }
+
+        this.render()
     }
 
-    extend(point) {
+    extend(point) { }
 
-    }
+    render() {
 
-    render(context) {
-        ctx.moveTo(0, 0);
-        ctx.lineTo(200, 100);
-        ctx.stroke();
+        this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
+        this.context.beginPath()
+
+        this.context.moveTo(...this.locate('A'));
+        this.context.lineTo(...this.locate('B'));
+
+        this.context.moveTo(...this.locate('B'));
+        this.context.lineTo(...this.locate('C'));
+
+        this.context.moveTo(...this.locate('C'));
+        this.context.lineTo(...this.locate('D'));
+
+        this.context.moveTo(...this.locate('D'));
+        this.context.lineTo(...this.locate('A'));
+
+        this.context.stroke();
     }
 }
-
-const rect = new Rectangle(
-    {
-        x: 100,
-        y: 100
-    },
-    {
-        x: 600,
-        y: 100
-    },
-    {
-        x: 100,
-        y: 400
-    },
-    {
-        x: 600,
-        y: 400
-    })
-
-const canvas = document.getElementById('myCanvas')
-const context = canvas.getContext("2d")
-
-rect.render(context)
 
