90b7b8f6d3bc3d9cdaf9accbe3768bb3fbde9a2e
Truman Purnell <truman.purnell@gmail.com>
2018-04-11 12:41:05 -0400

    retools the param to fit with vector class

--- js/transformer/utils.js
+++ js/transformer/utils.js
@@ -8,48 +8,48 @@ const nsin = angle => sin(-angle)
 const ROTATION = {
 
     X: new Matrix([
-        [1, 0, 0, 0],
-        [0, cos, sin, 0],
-        [0, nsin, cos, 0],
-        [0, 0, 0, 1]
+        new Vector(1, 0, 0, 0),
+        new Vector(0, cos, sin, 0),
+        new Vector(0, nsin, cos, 0),
+        new Vector(0, 0, 0, 1)
     ]),
 
     Y: new Matrix([
-        [cos, 0, sin, 0],
-        [0, 1, 0, 0],
-        [nsin, 0, cos, 0],
-        [0, 0, 0, 1]
+        new Vector(cos, 0, sin, 0),
+        new Vector(0, 1, 0, 0),
+        new Vector(nsin, 0, cos, 0),
+        new Vector(0, 0, 0, 1)
     ]),
 
     Z: new Matrix([
-        [cos, sin, 0, 0],
-        [nsin, cos, 0, 0],
-        [0, 0, 1, 0],
-        [0, 0, 0, 1]
+        new Vector(cos, sin, 0, 0),
+        new Vector(nsin, cos, 0, 0),
+        new Vector(0, 0, 1, 0),
+        new Vector(0, 0, 0, 1)
     ])
 
 }
 
 const TRANSLATION = new Matrix([
-    [1, 0, 0, c],
-    [0, 1, 0, c],
-    [0, 0, 1, c],
-    [0, 0, 0, 1]
+    new Vector(1, 0, 0, c),
+    new Vector(0, 1, 0, c),
+    new Vector(0, 0, 1, c),
+    new Vector(0, 0, 0, 1)
 ])
 
 const CAMERA = {
     orthographic: new Matrix([
-        [c, 0, 0, 0],
-        [0, c, 0, 0],
-        [0, 0, c, c],
-        [0, 0, 0, 1]
+        new Vector(c, 0, 0, 0),
+        new Vector(0, c, 0, 0),
+        new Vector(0, 0, c, c),
+        new Vector(0, 0, 0, 1)
     ]),
 
     projection: new Matrix([
-        [c, 0, 0, 0],
-        [0, c, 0, 0],
-        [0, 0, c, c],
-        [0, 0, -1, 0]
+        new Vector(c, 0, 0, 0),
+        new Vector(0, c, 0, 0),
+        new Vector(0, 0, c, c),
+        new Vector(0, 0, -1, 0)
     ]),
 
 }
