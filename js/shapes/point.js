8a36ee17c878eb00e1ce58e8c442dba0162efab5
Truman Purnell <truman.purnell@gmail.com>
2018-04-10 23:24:16 -0400

    reconstructs point and vector classes to mirror graphics class

--- js/shapes/point.js
+++ js/shapes/point.js
@@ -1,156 +1,54 @@
-const AXIS = { X: 0, Y: 1 }
 
 class Point {
 
     /**
      * Creates an instance of Point.
-     * @param {...Double} dims 
+     * @param {any} coordinates 
      * @memberof Point
      */
-    constructor(...dims) {
-        this.context = SCENE.context
-        this.coordinates = dims
-        // 5 -> radius | 0 -> startAngle | Math.PI * 2 -> endAngle 
-        this.displayOpts = [5, 0, Math.PI * 2]
+    constructor(...coordinates) {
+        this.coordinates = coordinates
     }
 
     /**
-     * Create a new Point instance at location from MouseEvent
+     * Retrieves a value along a particular axis
      * 
-     * @static
-     * @param {MouseEvent} event 
-     * @returns A new Point with coordinates specified by event
+     * @param {any} axis 
+     * @returns 
      * @memberof Point
      */
-    static createFrom(mouseEvent) {
-        return new Point(mouseEvent.clientX, mouseEvent.clientY)
+    get(axis) {
+        return this.coordinates[axis]
     }
 
     /**
-    * Computes the midpoint of self and point
-    * 
-    * @param {Point} b 
-    * @memberof Point
-    */
-    static midpoint(a, b) {
+     * Adds a vector to a point
+     * 
+     * @param {any} vector 
+     * @returns {Point}
+     * @memberof Point
+     */
+    addVector(vector) {
         return new Point(
-            ...a.coordinates.map(function (coordinate, axis) {
-                return (coordinate + b.coordinates[axis]) / 2
+            ...this.coordinates.map(function (ci, i) {
+                return ci + vector.get(i)
             })
         )
     }
 
     /**
-     * Calculates the distance between self and point
+     * Subtracts two points returning a vector
      * 
-     * @param {Point} point
-     * @returns 
+     * @param {any} point 
+     * @returns {Vector}
      * @memberof Point
      */
-    distanceTo(point) {
-        return Math.sqrt(
-            this.diff(point).reduce(function (distance, difference) {
-                return distance + Math.pow(difference, 2)
-            }, 0)
+    subtractPoint(point) {
+        return new Vector(
+            ...this.coordinates.map(function (ci, i) {
+                return ci - point.get(i)
+            })
         )
     }
 
-    /**
-     * Shifts the point by provided offsets
-     * 
-     * @param {Array} offsets 
-     * @returns 
-     * @memberof Point
-     */
-    shift(...offsets) {
-        if (offsets.length !== this.coordinates.length)
-            throw new Error('Dims must be equal!')
-
-        this.coordinates = this.coordinates.map(function (val, dim) {
-            return val + offsets[dim]
-        })
-
-        return this
-    }
-
-    /**
-     * Multiply each coordinate by specified factor
-     * 
-     * @param {Double} factor 
-     * @memberof Point
-     */
-    scale(factor) {
-        this.coordinates = this.coordinates.map(function (coordinate) {
-            return coordinate * factor
-        })
-
-        return this
-    }
-
-    /**
-     * Compute the difference between two points' coordinates
-     * 
-     * @param {Point} point 
-     * @returns The differences in coordinate value along each axis
-     * @memberof Point
-     */
-    diff(point) {
-        return this.coordinates.map(function (val, dim) {
-            return val - point.coordinates[dim]
-        })
-    }
-
-    /**
-     * Returns the two corners forming a rectangle with the passed point
-     * 
-     * @param {Point} point 
-     * @returns 
-     * @memberof Point
-     */
-    corners(point) {
-        return [
-            new Point(point.coordinates[AXIS.X], this.coordinates[AXIS.Y]),
-            new Point(this.coordinates[AXIS.X], point.coordinates[AXIS.Y]),
-        ]
-    }
-
-    /**
-     * Renders the Point as a circle
-     * 
-     * @memberof Point
-     */
-    render() {
-        this.context.beginPath()
-        this.context.moveTo(...this.coordinates)
-        this.context.arc(...this.coordinates, ...this.displayOpts)
-        this.context.stroke()
-
-        return this
-    }
-
-    /**
-     * Checks whether the point is within the given Polygon
-     * 
-     * @param {Polygon} polygon 
-     * @returns 
-     * @memberof Point
-     */
-    isWithin(polygon) {
-        const center = polygon.computeCenter()
-        const distanceToCenter = this.distanceTo(center)
-
-        return polygon.vertices.every(function (vertex) {
-            return vertex.distanceTo(center) >= distanceToCenter
-        })
-    }
-
-    /**
-     * Returns a clone of this Point instance
-     * 
-     * @returns 
-     * @memberof Point
-     */
-    clone() {
-        return new Point(...this.coordinates)
-    }
 }
