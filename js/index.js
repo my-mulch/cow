f6ab63877024742c5ced6d7748f1adb912ae7a64
Truman Purnell <truman.purnell@gmail.com>
2018-06-02 18:01:21 -0700

    successfully renders a cube

--- js/index.js
+++ js/index.js
@@ -2,4 +2,30 @@ const keyboard = new Keyboard()
 const mouse = new Mouse()
 const scene = new Scene()
 
-scene.add(new Point(300, 300))
+const cube = new Solid(
+    vertices = [
+        new Point(100, 100, 100),
+        new Point(100, 100, 200),
+        new Point(100, 200, 100),
+        new Point(100, 200, 200),
+        new Point(200, 100, 100),
+        new Point(200, 100, 200),
+        new Point(200, 200, 100),
+        new Point(200, 200, 200),
+    ],
+    edges = [
+        [0, 1],
+        [0, 2],
+        [0, 4],
+        [1, 3],
+        [1, 5],
+        [2, 3],
+        [2, 6],
+        [3, 7],
+        [4, 5],
+        [4, 6],
+        [5, 7],
+        [6, 7]
+    ]
+)
+scene.add(cube)
