3060f159a840395e791693c760686091cb835d17
Truman Purnell <truman.purnell@gmail.com>
2018-06-09 00:08:26 -0600

    removes preventdefault on keypress not in bindings

--- front/io/keyboard.js
+++ front/io/keyboard.js
@@ -1,3 +1,4 @@
+
 class Keyboard {
     constructor(bindings, context = window) {
         this.context = context
@@ -5,17 +6,16 @@ class Keyboard {
         this.pressedKeys = new Set()
 
         this.keyPressedAction = function (event) {
-            event.preventDefault()
+            if (event.key in this.bindings) event.preventDefault()
             this.pressedKeys.add(event.key)
         }
 
         this.keyReleasedAction = function (event) {
-            event.preventDefault()
+            if (event.key in this.bindings) event.preventDefault()
             this.pressedKeys.delete(event.key)
         }
 
         this.context.addEventListener("keydown", this.keyPressedAction.bind(this))
         this.context.addEventListener("keyup", this.keyReleasedAction.bind(this))
-
     }
 }
