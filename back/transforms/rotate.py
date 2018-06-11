8ce2b2c53b1d1027b953f24491627998260d9f24
Truman Purnell <truman.purnell@gmail.com>
2018-06-10 22:18:48 -0600

    refactor complete for action calls in server

--- back/transforms/rotate.py
+++ back/transforms/rotate.py
@@ -4,12 +4,14 @@ from transforms.compose import compose
 import numpy as np
 
 
-def rotate_in_place(axis_of_rotation):
+def rotate_in_place(shape, axis_of_rotation):
     # hard coded angle -- eventually replace
-    rotate = axis_of_rotation(np.pi / 128)
+    rotate = axis_of_rotation(np.pi / 32)
     move_to_origin, move_to_initial = translate_to_origin_and_back(shape)
+    composition = compose(move_to_initial, rotate, move_to_origin).T
+    shape['vertices'] = np.array(shape['vertices']).dot(composition).tolist()
 
-    return compose(move_to_initial, rotate, move_to_origin)
+    return shape
 
 
 def rotX(theta):
@@ -46,3 +48,15 @@ def rotZ(theta):
         [0, 0, 1, 0],
         [0, 0, 0, 1]
     ])
+
+
+def rotX_in_place(shape):
+    return rotate_in_place(shape, rotX)
+
+
+def rotY_in_place(shape):
+    return rotate_in_place(shape, rotY)
+
+
+def rotZ_in_place(shape):
+    return rotate_in_place(shape, rotZ)
