6e8a8b5145a4e7af05e8d70d60be39a55a1b70cf
Truman Purnell <truman.purnell@gmail.com>
2018-07-03 16:11:10 -0700

    broken: strips transform calls from scene.js

--- front/display/scene.js
+++ front/display/scene.js
@@ -13,28 +13,7 @@ class Scene {
         this.mouse = mouse
         this.objects = []
 
-        this.addActionListeners()
         window.setInterval(this.render.bind(this))
-    }
-
-    runActionsFromKeys() {
-        this.keyboard.pressedKeys.forEach(function (key) {
-            this.socket.emit(this.keyboard.bindings[key], JSON.stringify(this.objects))
-        }, this)
-    }
-
-    addActionListeners() {
-        for (let key in this.keyboard.bindings)
-            this.socket.on(this.keyboard.bindings[key], this.update.bind(this))
-    }
-
-    update(newSceneObjects) {
-        this.objects = newSceneObjects.map(function (object) {
-            // Convert each vertex coordinate array to a Point instance
-            return new Solid(object.vertices.map(function (vertex) {
-                return new Point(...vertex)
-            }), object.edges)
-        }, this)
     }
 
     add(object) {
@@ -42,10 +21,17 @@ class Scene {
     }
 
     render() {
-        this.runActionsFromKeys()
-        this.context.clearRect(0, 0, this.width, this.height)
-        this.context.beginPath()
-        this.objects.forEach(function (object) { object.render() })
-        this.context.stroke()
+        const actions = this.keyboard.pressedKeys.getActions()
+
+        if (actions) {
+            this.context.clearRect(0, 0, this.width, this.height)
+            this.context.beginPath()
+
+            for (const action of actions)
+                for (const object of this.objects)
+                    object.apply(action).render()
+
+            this.context.stroke()
+        }
     }
 }
