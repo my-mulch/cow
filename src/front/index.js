2eec777b4d19ecf6502d298f487ce7644f3d0292
Truman Purnell <truman.purnell@gmail.com>
2018-07-29 00:46:39 -0500

    transforms back to webserver -- first successful np array flow to front end

--- src/front/index.js
+++ src/front/index.js
@@ -2,75 +2,75 @@ import Scene from './display/scene'
 import Solid from './display/solid'
 import Point from './display/point'
 import Keyboard from './io/keyboard'
+import ndim from 'multi-dim'
 
 
-const PORT = 5000
+const PORT = 3000
 const DOMAIN = 'localhost'
-const CONNSTR = `http://${DOMAIN}:${PORT}/`
+const CONNSTR = `ws://${DOMAIN}:${PORT}/`
 
+const socket = new WebSocket(CONNSTR)
+socket.addEventListener('message', function (event) {
+    console.log(ndim.array(JSON.parse(event.data)).toString())
+})
 
-// const scene = new Scene({
-//     // socket: io.connect(CONNSTR),
-//     canvas: document.getElementById('canvas'),
-//     keyboard: new Keyboard({
-//         // Bindings if you aint know
-//         'q': 'rotateX',
-//         'w': 'rotateY',
-//         'e': 'rotateZ',
-//     }),
-// })
+const scene = new Scene({
+    socket: socket,
+    canvas: document.getElementById('canvas'),
+    keyboard: new Keyboard({}),
+})
 
-// const cube = new Solid(
-//     [
-//         new Point(100, 100, 100, 1),
-//         new Point(100, 100, 200, 1),
-//         new Point(100, 200, 100, 1),
-//         new Point(100, 200, 200, 1),
-//         new Point(200, 100, 100, 1),
-//         new Point(200, 100, 200, 1),
-//         new Point(200, 200, 100, 1),
-//         new Point(200, 200, 200, 1),
-//     ],
-//     [
-//         [0, 1],
-//         [0, 2],
-//         [0, 4],
-//         [1, 3],
-//         [1, 5],
-//         [2, 3],
-//         [2, 6],
-//         [3, 7],
-//         [4, 5],
-//         [4, 6],
-//         [5, 7],
-//         [6, 7],
-//     ]
-// )
+const cube = new Solid(
+    [
+        new Point(100, 100, 100, 1),
+        new Point(100, 100, 200, 1),
+        new Point(100, 200, 100, 1),
+        new Point(100, 200, 200, 1),
+        new Point(200, 100, 100, 1),
+        new Point(200, 100, 200, 1),
+        new Point(200, 200, 100, 1),
+        new Point(200, 200, 200, 1),
+    ],
+    [
+        [0, 1],
+        [0, 2],
+        [0, 4],
+        [1, 3],
+        [1, 5],
+        [2, 3],
+        [2, 6],
+        [3, 7],
+        [4, 5],
+        [4, 6],
+        [5, 7],
+        [6, 7],
+    ]
+)
 
-// const triangle = new Solid(
-//     [
-//         new Point(400, 400, 0, 1),
-//         new Point(300, 400, 0, 1),
-//         new Point(400, 500, 0, 1),
-//     ],
-//     [
-//         [1, 2],
-//         [2, 0],
-//         [0, 1],
-//     ]
-// )
+const triangle = new Solid(
+    [
+        new Point(400, 400, 0, 1),
+        new Point(300, 400, 0, 1),
+        new Point(400, 500, 0, 1),
+    ],
+    [
+        [1, 2],
+        [2, 0],
+        [0, 1],
+    ]
+)
 
-// const line = new Solid(
-//     [
-//         new Point(240, 375, 0, 1),
-//         new Point(240, 600, 0, 1),
-//     ],
-//     [
-//         [0, 1],
-//     ]
-// )
+const line = new Solid(
+    [
+        new Point(240, 375, 0, 1),
+        new Point(240, 600, 0, 1),
+    ],
+    [
+        [0, 1],
+    ]
+)
 
-// scene.add(cube)
-// scene.add(triangle)
-// scene.add(line)
+scene.add(cube)
+scene.add(triangle)
+scene.add(line)
 
