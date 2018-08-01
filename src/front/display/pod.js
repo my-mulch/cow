7275e1109d89c9be07464e167b76df505c0b5a1f
Truman Purnell <truman.purnell@gmail.com>
2018-07-31 23:35:50 -0500

    adds createFrom method within Pod to handle socket data integration with scene

--- src/front/display/pod.js
+++ src/front/display/pod.js
@@ -1,9 +1,26 @@
+import Point from '../draw/point'
+import nd from 'multi-dim'
+
 export default class Pod {
     constructor(props) {
         this.height = props.height
         this.width = props.width
+        this.depth = props.depth
         this.origin = props.origin
-        this.points = props.points
+
+        this.data = props.data
         this.edges = props.edges
     }
+
+    static createFrom(socketMessage) {
+        return new Pod({
+            height: 200,
+            width: 200,
+            depth: 200,
+            origin: new Point(0, 0, 0, 1),
+            data: nd.array(JSON.parse(socketMessage.data)),
+            edges: null
+        })
+    }
+
 }
--- src/front/display/scene.js
+++ src/front/display/scene.js
@@ -3,6 +3,7 @@ import Pod from './pod'
 export default class Scene {
 
     constructor(props) {
+        this.pods = []
         this.canvas = props.canvas
         this.context = this.canvas.getContext('2d')
 
@@ -10,11 +11,15 @@ export default class Scene {
         this.mouse = props.mouse || null
         this.socket = props.socket || null
 
-        this.socket.getInstance().addEventListener('message', Pod.create)
+        this.socket.getInstance().addEventListener('message', function (message) {
+            this.pods.push(Pod.createFrom(message))
+        }, this)
 
         this.width = this.canvas.clientWidth
         this.height = this.canvas.clientHeight
     }
 
-    render() { }
+    render() {
+        
+    }
 }
