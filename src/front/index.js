a4fb9a656632ec349467c802bf33b33ce9e0f61d
Truman Purnell <truman.purnell@gmail.com>
2018-08-07 22:49:43 -0500

    adds illustrator back to handle rendering of pods

--- src/front/index.js
+++ src/front/index.js
@@ -2,10 +2,12 @@ import Scene from './display/scene'
 import Keyboard from './io/keyboard'
 import Socket from './io/socket'
 import Mouse from './io/mouse'
+import Illustrator from './display/illustrator'
 
 new Scene({
     socket: new Socket(3000, 'localhost'),
     canvas: document.getElementById('canvas'),
     keyboard: new Keyboard({ /* Key Bindings */ }),
-    mouse: new Mouse()
+    mouse: new Mouse(),
+    renderer: Illustrator
 })
