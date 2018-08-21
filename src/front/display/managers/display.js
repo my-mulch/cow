7587ced7b6c215887ee1b3f058dc2e4c8ef51d0b
Truman Purnell <truman.purnell@gmail.com>
2018-08-21 00:03:14 -0500

    pseudo code for the merging of POD managers

--- src/front/display/managers/display.js
+++ src/front/display/managers/display.js
@@ -1,1 +1,17 @@
-export default class DisplayManager { }
+export default class DisplayManager {
+    constructor(display) {
+        this.display = display
+
+        this.getExecutorAndBind = this.getExecutorAndBind.bind(this)
+    }
+
+    getExecutorAndBind(data) {
+        return function (resolve, reject) {
+            setTimeout(resolve, this.display.animationPause, data)
+        }
+    }
+
+    step(data) {
+        return new Promise(this.getExecutorAndBind(data))
+    }
+}
--- src/front/media/scatter.js
+++ src/front/media/scatter.js
@@ -1,29 +1,10 @@
 export default class ScatterMedia {
     constructor(ndArray) {
         this.media = ndArray.toGenerator()
-        this.animateWait = 0
-
-        this.executor = this.executor.bind(this)
     }
 
-    executor(resolve) {
-        setTimeout(resolve, this.animateWait, this.media.next().value)
-    }
-
-    step() {
-        return new Promise(this.executor)
-    }
-
-    async render(scene, layout, point = null) {
-        while (point = await this.step()) {
-            const newPoint = layout.locate(point)
-
-            scene.context.fillRect(
-                newPoint.slice(0),
-                newPoint.slice(1),
-                1, 1
-            )
-        }
+    async render(scene) {
+        scene.context.fillRect(/* Put data in me! */)
     }
 
     static matches(ndArray) {
