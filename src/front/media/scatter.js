a038ede75f7de11482aee992377552f47e572606
Truman Purnell <truman.purnell@gmail.com>
2018-08-22 00:16:25 -0500

    implements render in ScatterMedia with pseudocode

--- src/front/media/scatter.js
+++ src/front/media/scatter.js
@@ -4,7 +4,37 @@ export default class ScatterMedia {
     }
 
     async render(scene, layout, playback) {
-        scene.context.fillRect(/* Put data in me! */)
+        playback.start()
+
+        const shifts = this.media.min(axis = 0)
+        const shift = layout.transform(
+            shifts.slice(0),
+            shifts.slice(1),
+            shifts.slice(2))
+
+        const scales = nd.norm(this.media.max(axis = 1))
+        const scaleDown = layout.scale(
+            scales.slice(0),
+            scales.slice(1),
+            scales.slice(2))
+
+        const scaleUp = layout.scale(
+            layout.size.X,
+            layout.size.Y,
+            layout.size.Z)
+
+        const layoutMatrix = nd.compose(shift, scaleDown, scaleUp)
+        const formattedMedia = layoutMatrix.dot(this.media)
+
+        const genMedia = formattedMedia.toGenerator(axis = 0)
+        let point
+
+        while (point = genMedia.next().value) {
+            if (playback.isAnimated)
+                playback.animationWait()
+
+            scene.context.fillRect(point.slice(0), point.slice(1), 1, 1)
+        }
     }
 
     static matches(ndArray) {
