5d3c11cae5dab893f147f4566d9715717da63849
Truman Purnell <truman.purnell@gmail.com>
2018-09-09 17:39:34 -0700

    color image data displayed spatially

--- src/front/media/scatter.js
+++ src/front/media/scatter.js
@@ -6,40 +6,35 @@ export default class ScatterMedia {
     }
 
     async render(scene, layout, playback) {
-        // playback.start()
         const shifts = this.media.mean(0, 1)
         const shift = layout.transform(
             -shifts.slice(0),
             -shifts.slice(1),
             -shifts.slice(2))
 
+        const rotate = layout.rotate(Math.PI / 4)
+        console.log(rotate.toString())
         const maxLength = this.media.norm(2).max()
-        const scaleDown = layout.scale(
-            1 / maxLength,
-            1 / maxLength,
-            1 / maxLength)
+        const scale = layout.scale(
+            layout.size.X / maxLength,
+            layout.size.Y / maxLength,
+            layout.size.Z / maxLength)
 
-        const scaleUp = layout.scale(
-            layout.size.X,
-            layout.size.Y,
-            layout.size.Z)
-
-        const moveToLocation = layout.transform(
+        const relocate = layout.transform(
             layout.origin.X,
             layout.origin.Y,
             0)
 
-
-        const layoutMatrix = nd.dot(shift, scaleDown, scaleUp, moveToLocation)
-        const formattedMedia = this.media.reshape(144000, 4).dot(layoutMatrix)
+        const layoutMatrix = nd.dot(shift, rotate, scale, relocate)
+        const reshapedMedia = this.media.reshape(144000, 4)
+        const formattedMedia = reshapedMedia.dot(layoutMatrix)
 
         for (let i = 0; i < 144000; i++) {
             const [x, y, z, w] = formattedMedia.slice(i).toRawArray()
+            const [r, g, b, a] = reshapedMedia.slice(i).toRawArray()
+            scene.context.fillStyle = `rgb(${r}, ${g}, ${b})`
             scene.context.fillRect(x, y, 1, 1)
-
         }
-
-        // playback.stop()
     }
 
     static matches(ndArray) {
