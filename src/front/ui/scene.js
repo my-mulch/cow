573c132d1119744d23e778fe26a9170960d7c653
Truman Purnell <truman.purnell@gmail.com>
2018-09-11 08:54:36 -0500

    implements template for mouseevent functions

--- src/front/ui/scene.js
+++ src/front/ui/scene.js
@@ -1,12 +1,14 @@
+import Mouse from '../io/mouse'
+import Keyboard from '../io/keyboard'
+
 export default class Scene {
 
     constructor(props) {
         this.canvas = props.canvas || document.getElementById('canvas')
         this.context = this.canvas.getContext('2d')
 
-        this.canvas.addEventListener('mousedown', console.log)
-        this.canvas.addEventListener('mouseup', console.log)
-        this.canvas.addEventListener('mousemove', console.log)
+        this.mouse = props.mouse || new Mouse(this)
+        // this.keyboard = props.keyboard || new Keyboard(this)
 
         this.width = this.canvas.clientWidth
         this.height = this.canvas.clientHeight
