be81aeaa5e89edea3e28f6d9ae9a88b39d2458d2
Truman Purnell <truman.purnell@gmail.com>
2018-09-11 08:27:31 -0500

    scene no longer has mouse and keyboard classes --- instead events and data will be passed into these classes

--- src/front/io/mouse.js
+++ src/front/io/mouse.js
@@ -1,9 +1,5 @@
 export default class Mouse {
     constructor(context = window) {
         this.context = context
-
-        this.context.addEventListener('mousedown', function (mouseEvent) { })
-        this.context.addEventListener('mouseup', function (mouseEvent) { })
-        this.context.addEventListener('mousemove', function (event) { })
     }
 }
