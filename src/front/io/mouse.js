fa6408b8b1b62a3d2abffb5b9becd96b35ad72cb
Truman Purnell <truman.purnell@gmail.com>
2018-09-11 08:54:36 -0500

    implements template for mouseevent functions

--- src/front/io/mouse.js
+++ src/front/io/mouse.js
@@ -1,5 +1,31 @@
 export default class Mouse {
-    constructor(context = window) {
-        this.context = context
+    constructor(scene) {
+        this.scene = scene
+        this.isPressed = false
+        this.selectedPod = null
+
+        this.scene.canvas.addEventListener('mousedown', this.clickDown.bind(this))
+        this.scene.canvas.addEventListener('mouseup', this.clickUp.bind(this))
+        this.scene.canvas.addEventListener('mousemove', this.move.bind(this))
+    }
+
+    clickDown(clickEvent) {
+        this.isPressed = true
+
+        for (const pod of this.scene.pods)
+            if (pod.isWithin(clickEvent)) {
+                this.selectedPod = pod
+                break
+            }
+    }
+
+    clickUp(event) {
+        this.isPressed = false
+        this.selectedPod = false
+    }
+
+    move(moveEvent) {
+        if (this.selectedPod)
+            this.selectedPod.move(moveEvent)
     }
 }
