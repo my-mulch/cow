b1632db5769f0a72586cfc6ad176ca2b55e306b1
Truman Purnell <truman.purnell@gmail.com>
2018-09-13 18:53:43 -0500

    rotation in place now complete

--- src/front/io/keyboard.js
+++ src/front/io/keyboard.js
@@ -3,33 +3,23 @@ export default class Keyboard {
         this.pressedKeys = new Set()
         this.scene = props.scene
 
-        window.addEventListener("keyup", (function () { this.pressedKeys = new Set() }).bind(this))
-        window.addEventListener("keydown", (function (event) { this.pressedKeys.add(event.key) }).bind(this))
+        this.run = this.run.bind(this)
+
+        window.addEventListener("keyup", this.keyU.bind(this))
+        window.addEventListener("keydown", this.keyD.bind(this))
     }
 
-    [''](pod) { /* NOOP */ }
-    [' '](pod) { console.log('hi space!') }
-
-    ['a'](pod) { pod.rotate(Math.PI / 256, 'x') }
-    ['s'](pod) { pod.rotate(Math.PI / 256, 'y') }
-    ['d'](pod) { pod.rotate(Math.PI / 256, 'z') }
+    ['a'](pod) { pod.rotate(Math.PI / 1024, 'x') }
+    ['s'](pod) { pod.rotate(Math.PI / 1024, 'y') }
+    ['d'](pod) { pod.rotate(Math.PI / 1024, 'z') }
 
     ['ArrowLeft'](pod) { pod.translate(-10, 0, 0, 0) }
     ['ArrowRight'](pod) { pod.translate(10, 0, 0, 0) }
     ['ArrowUp'](pod) { pod.translate(0, -10, 0, 0) }
     ['ArrowDown'](pod) { pod.translate(0, 10, 0, 0) }
 
+    keyU(event) { this.pressedKeys = new Set() }
+    keyD(event) { this.pressedKeys.add(event.key) }
 
-
-    run(pod) {
-        this.scene.context.clearRect(0, 0, this.scene.width, this.scene.height)
-
-        this[Array
-            .from(this.pressedKeys)
-            .sort()
-            .join()](pod)
-
-        pod.render(this.scene)
-        this.scene.context.stroke()
-    }
+    run(pod) { return key => this[key](pod) }
 }
