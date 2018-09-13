0e1db2ac34db3c74a6caaff5fe2e210a7d1f9d9e
Truman Purnell <truman.purnell@gmail.com>
2018-09-12 23:12:36 -0500

    removes silly instrospection method from keyboard

--- src/front/io/keyboard.js
+++ src/front/io/keyboard.js
@@ -2,7 +2,6 @@ export default class Keyboard {
     constructor(props) {
         this.pressedKeys = new Set()
         this.scene = props.scene
-        this.bindings = this.makeFunctionsTheBindings()
 
         window.addEventListener("keyup", (function () { this.pressedKeys = new Set() }).bind(this))
         window.addEventListener("keydown", (function (event) { this.pressedKeys.add(event.key) }).bind(this))
@@ -13,21 +12,13 @@ export default class Keyboard {
     ['a'](pod) { pod.rotate(Math.PI / 16, 'x') }
     ['s'](pod) { pod.rotate(Math.PI / 16, 'y') }
     ['d'](pod) { pod.rotate(Math.PI / 16, 'z') }
+    [' '](pod) { console.log('hi space!') }
 
 
     run(pod) {
-        this[Array.from(this.pressedKeys)
+        this[Array
+            .from(this.pressedKeys)
             .sort()
             .join()](pod)
-    }
-
-    makeFunctionsTheBindings() {
-        const functionAssociator = function (bindings, action) {
-            return Object.assign(bindings, { action: this[action] })
-        }
-
-        return Object
-            .getOwnPropertyNames(this.__proto__)
-            .reduce(functionAssociator.bind(this), {})
     }
 }
