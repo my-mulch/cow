baaf503c6eca205928e9656e8484a04b690d6d75
Truman Purnell <truman.purnell@gmail.com>
2018-09-13 19:57:09 -0500

    implements reposition method in layout

--- src/front/media/scatter.js
+++ src/front/media/scatter.js
@@ -5,39 +5,17 @@ export default class ScatterMedia {
         this.media = ndArray
     }
 
-    async render(scene, layout, playback) {
-        const shifts = this.media.mean(0, 1)
-        const shift = layout.transform(
-            -shifts.slice(0),
-            -shifts.slice(1),
-            -shifts.slice(2))
+    async render(scene, layout, playback, display) {
+        const displayMedia = layout.reposition(this.media)
+        const originalMedia = this.media.reshape(-1, this.media.header.shape.slice().pop())
 
-        const rotate = layout.rotate(Math.PI / 6)
+        for (let i = 0; i < displayMedia.header.shape[0].length; i++) {
+            const [x, y, z, w] = displayMedia.slice(i)
+            const [r, g, b, a] = originalMedia.slice(i)
 
-        const maxLength = this.media.norm(2).max()
-        const scale = layout.scale(
-            layout.size.X / maxLength,
-            layout.size.Y / maxLength,
-            layout.size.Z / maxLength)
-
-        const relocate = layout.transform(
-            layout.origin.X,
-            layout.origin.Y,
-            0)
-
-        const layoutMatrix = nd.dot(shift, rotate, scale, relocate)
-        const reshapedMedia = this.media.reshape(-1, 4)
-        const formattedMedia = reshapedMedia.dot(layoutMatrix)
-
-        for (let i = 0; i < reshapedMedia.header.shape[0]; i++) {
-            const [x, y, z, w] = formattedMedia.slice(i).toRawArray()
-            const [r, g, b, a] = reshapedMedia.slice(i).toRawArray()
             scene.context.fillStyle = `rgb(${r}, ${g}, ${b})`
             scene.context.fillRect(x, y, 1, 1)
         }
     }
-
-    static matches(ndArray) {
-        return ndArray.data instanceof Float64Array
-    }
+    
 }
