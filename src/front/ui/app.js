e44d826363301232fe46fcf1a605d524e4d2a63a
Truman Purnell <truman.purnell@gmail.com>
2018-09-14 18:35:28 -0500

    implements video feed source with stop and start

--- src/front/ui/app.js
+++ src/front/ui/app.js
@@ -2,19 +2,21 @@
 export default class App {
     constructor(components) {
         this.scene = components.scene
-        this.camera = components.camera
 
+        this.camera = components.camera
         this.fileDrop = components.fileDrop
         this.socket = components.socket
 
         this.fileDrop.onData = this.addPodFromFileDrop.bind(this)
         this.socket.onData = this.addPodFromSocket.bind(this)
+        this.camera.onData = this.addPodFromCamera.bind(this)
 
         window.setInterval(this.render.bind(this))
     }
 
     addPodFromFileDrop(file) { console.log(file) }
     addPodFromSocket(file) { console.log(file) }
+    addPodFromCamera(file) { console.log(file) }
 
     render() { }
 }
