58e767746ccd89ac705f5639bf427964a2fe6d1b
Truman Purnell <truman.purnell@gmail.com>
2018-09-17 11:46:57 -0500

    terribly simple server setup for audio / video routing

--- src/back/index.mjs
+++ src/back/index.mjs
@@ -1,3 +1,14 @@
 'use strict'
 
-// Sample Server
+import express from 'express'
+import ServerHandlers from './utils'
+
+const port = process.env.PORT || 3000
+const app = express()
+
+app.listen(port, ServerHandlers.listen)
+
+app.post('/audio', console.log)
+app.post('/video', console.log)
+
+app.use('*', ServerHandlers.sendIndex)
