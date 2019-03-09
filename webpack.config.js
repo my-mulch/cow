b48ab145c4def18122b3a544e1bd2b97e9233962
trumanpurnell <truman.purnell@gmail.com>
2019-03-09 10:16:20 -0800

    adjusts paths to enable gh-pages load

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-    entry: './src/main/index.js',
+    entry: './src/main/front/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'docs')
