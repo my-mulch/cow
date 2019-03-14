fda2473b9b1b96661be86dec2b24c28e14e7ed09
trumanpurnell <truman.purnell@gmail.com>
2019-03-14 11:18:45 -0700

    deletes api keys

--- src/main/back/deploy/terraform/deploy.tf
+++ src/main/back/deploy/terraform/deploy.tf
@@ -3,16 +3,17 @@ locals {
   aws_role_arn = "arn:aws:iam::718734850255:role/service-role/admin"
   aws_handler  = "index.handler"
   aws_runtime  = "python3.6"
+  aws_region   = "us-west-1"
+  aws_profile  = "play"
 
   ###################################### Load-Image ##################################
   load_image_path = "/Users/tpurne984/Workspace/play/parmesan/src/main/back/load-image"
-  load_image_name = "load-image-test"
+  load_image_name = "parmesan-load-image"
 }
 
 provider "aws" {
-  access_key = "AKIAIFE6HTLEWJ52RYKQ"
-  secret_key = "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"
-  region     = "us-west-1"
+  region  = "${local.aws_region}"
+  profile = "${local.aws_profile}"
 }
 
 data "archive_file" "archive_load_image" {
@@ -22,10 +23,18 @@ data "archive_file" "archive_load_image" {
 }
 
 resource "aws_lambda_function" "lambda_load_image" {
-  filename         = "${local.load_image_path}/${local.load_image_name}.zip"
+  filename         = "${data.archive_file.archive_load_image.output_path}"
   function_name    = "${local.load_image_name}"
   role             = "${local.aws_role_arn}"
   handler          = "${local.aws_handler}"
   source_code_hash = "${data.archive_file.archive_load_image.output_base64sha256}"
   runtime          = "${local.aws_runtime}"
 }
+
+# resource "aws_lambda_alias" "lambda_load_image" {
+#   name             = "stage"
+#   description      = "this is my stage"
+#   function_name    = "${aws_lambda_function.lambda_load_image.arn}"
+#   function_version = "2"
+# }
+
