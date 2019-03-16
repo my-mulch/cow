3115011654b3a27b7c73b6bbd652b42d5edbdfc8
trumanpurnell <truman.purnell@gmail.com>
2019-03-16 01:45:47 -0700

    fixes path issues for load image lambda

--- src/main/deploy/scripts/deploy-parmesan.sh
+++ src/main/deploy/scripts/deploy-parmesan.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
 pip install numpy
-cp -r /var/lang/lib/python3.6/site-packages/numpy parmesan-lambdas/src/main/back/load_image
 
-
+cp -r /var/lang/lib/python3.6/site-packages/numpy parmesan-lambdas/src/main/back/load-image
 mv ~/.terraform parmesan-lambdas/src/main/deploy
-cd parmesan-lambdas/src/main/deploy
 
-terraform init
+terraform init parmesan-lambdas/src/main/deploy
 
 terraform plan \
 -out=parmesan-deploy-plan \
 -input=false \
 -var="lambda_name=parmesan-load-image" \
--var="lambda_path=parmesan-lambdas/src/main/back/load_image" \
+-var="lambda_path=parmesan-lambdas/src/main/back/load-image" \
 
 terraform apply \
 -input=false \
