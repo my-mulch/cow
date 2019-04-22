2a76dbfaad2a1fd84f690e09293bd018502bf1e5
trumanpurnell <truman.purnell@gmail.com>
2019-04-22 13:45:59 -0700

    adds correct fragment and vertex source to graphics example

--- src/main/front/gl/examples/colors.mjs
+++ src/main/front/gl/examples/colors.mjs
@@ -1,19 +1,19 @@
-var vertex =
+var V_SHADER_SOURCE =
     'attribute vec4 a_Position;\n' +
     'attribute vec4 a_Color;\n' +
-    'uniform mat4 u_ViewMatrix;\n' +
-    'varying vec4 v_Color;\n' +
+    'varying vec4 v_Color;\n' + // varying variable
     'void main() {\n' +
-    '  gl_Position = u_ViewMatrix * a_Position;\n' +
-    '  v_Color = a_Color;\n' +
+    '  gl_Position = a_Position;\n' +
+    '  gl_PointSize = 10.0;\n' +
+    '  v_Color = a_Color;\n' +  // Pass the data to the fragment shader
     '}\n';
 
 // Fragment shader program
-var fragment =
+var F_SHADER_SOURCE =
     '#ifdef GL_ES\n' +
-    'precision mediump float;\n' +
+    'precision mediump float;\n' + // Precision qualifier (See Chapter 6)
     '#endif\n' +
-    'varying vec4 v_Color;\n' +
+    'varying vec4 v_Color;\n' +    // Receive the data from the vertex shader
     'void main() {\n' +
     '  gl_FragColor = v_Color;\n' +
     '}\n';
