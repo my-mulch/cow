f9ca9300d8f116753258064f9254e860a5854659
Truman Purnell <truman.purnell@gmail.com>
2019-07-08 11:25:21 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-import bb from 'big-box'
-import app from './app'
-import myio from 'myio'
-
-window.bb = bb
-window.app = app
-window.myio = myio
-
-export default (async function () {
-    const img = await myio.imread('http://localhost:8000/Users/trumanpurnell/Desktop/sox.jpg')
-
-    const vertices = img.reshape({ shape: [-1, 4] })
-    const colors = vertices
-    const sizes = bb
-        .ones({ shape: [img.shape[0] * img.shape[1], 1] })
-        .multiply({ with: 1 })
-
-    app.graphics.plot({ vertices, colors, sizes })
-
-})()
+poop master
+joe
+joe mamma
